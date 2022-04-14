c03fdd96ba55fa5dc0cf306137ec2b637edd27d8
Sahil Garg <sahil.garg891@gmail.com>
2022-04-14 19:16:03 +0530

    Update

--- VHDL/cond_check.vhdl
+++ VHDL/cond_check.vhdl
@@ -3,7 +3,30 @@ use ieee.numeric_std.all;
 use ieee.math_real.all;
 use ieee.std_logic_1164.all;
 
-entity cc is
+entity cond_check is
     port(
-        
-    )
+      input: in std_logic_vector(15 downto 0);
+      output: out std_logic_vector(15 downto 0); 
+    );
+end entity;
+
+architecture arch of cond_check is
+    signal out_temp: std_logic_vector(2 downto 0) :="000";
+    signal out_inc: std_logic_vector(2 downto 0) :="000";
+
+    begin
+        cc: process(input)
+            begin
+                for i in 0 to 7 loop
+                    if input(i)='1' then
+                        out_temp <= out_inc;
+                    end if
+                    out_inc <=  out_inc + "001"; -- !!! Check for error here              
+                end loop;
+            end process;
+
+            output<= out_temp;
+end arch;
+
+
+
